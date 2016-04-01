0f13b50d00b162fb3c448782d487622662922d1c
barong <unc.lab@gmail.com>
2016-04-02 00:19:47 +0300

    fix webpack config merge. remove NODE_ENV variable in config

--- grunt/config/webpack.dev.config.js
+++ grunt/config/webpack.dev.config.js
@@ -1,12 +1,20 @@
 var path = require("path");
 var webpack = require("webpack");
 var extend = require('extend');
+var files = require('./files.path.config');
+var webpackFailPlugin = require('webpack-fail-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var config = require('./webpack.base.config');
 
-var devConfig = {
+module.exports = {
+    entry: {
+        "main-front": files.source_ts + '/app-front/main-front.tsx',
+        "main-back": files.source_ts + '/app-back/main-back.ts'
+    },
     devtool: 'eval-source-map',
     output: {
+        path: files.assets,
+        filename: '[name].js',
         publicPath: '/assets/'
     },
     devServer: {
@@ -26,6 +34,44 @@ var devConfig = {
             }
         }
     },
+    resolve: {
+        extensions: ['', '.ts', '.tsx', '.js', '.less'],
+        modulesDirectories: [
+            './node_modules',
+            files.js_resources,
+            files.source_ts,
+            files.resources
+        ],
+        alias: {
+            'jquery': 'jquery/jquery-2.1.3.min',
+            'json': 'json/json2.min',
+            'underscore': 'underscore/underscore',
+            'backbone': 'backbone/backbone',
+            'fileupload': 'fileupload/jquery.fileupload',
+            // unescape module name with \\
+            'jquery.ui.widget': '\\jquery/jquery.ui.widget',
+            'iframe.transport': '\\jquery/jquery.iframe-transport',
+            'log4javascript': 'log4javascript/log4javascript.js'
+        }
+
+    },
+    // what comes as external dependencies
+    externals: {
+        "jquery": "$",
+        "backbone": "Backbone"
+    },
+    plugins: [
+        // The ProvidePlugin replaces a symbol in another source through the respective import
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+            React: "react"
+        }),
+        // Webpack plugin that will make the process return status code 1 when it finishes with errors in single-run mode.
+        webpackFailPlugin,
+        new ExtractTextPlugin("[name].css")
+    ],
     module: {
         loaders: [
             {
@@ -34,9 +80,11 @@ var devConfig = {
                     'react-hot',
                     'ts-loader'
                 ]
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader?sourceMap!less-loader?sourceMap'
             }
         ]
     }
 };
-
-module.exports = extend(true, config, devConfig);
--- grunt/config/webpack.prod.config.js
+++ grunt/config/webpack.prod.config.js
@@ -1,11 +1,70 @@
 var path = require("path");
 var webpack = require("webpack");
 var extend = require('extend');
+var files = require('./files.path.config');
+var webpackFailPlugin = require('webpack-fail-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-var config = require('./webpack.base.config');
 
-var prodConfig = {
-    
+module.exports = {
+    entry: {
+        "main-front": files.source_ts + '/app-front/main-front.tsx',
+        "main-back": files.source_ts + '/app-back/main-back.ts'
+    },
+    output: {
+        path: files.assets,
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['', '.ts', '.tsx', '.js', '.less'],
+        modulesDirectories: [
+            './node_modules',
+            files.js_resources,
+            files.source_ts,
+            files.resources
+        ],
+        alias: {
+            'jquery': 'jquery/jquery-2.1.3.min',
+            'json': 'json/json2.min',
+            'underscore': 'underscore/underscore',
+            'backbone': 'backbone/backbone',
+            'fileupload': 'fileupload/jquery.fileupload',
+            // unescape module name with \\
+            'jquery.ui.widget': '\\jquery/jquery.ui.widget',
+            'iframe.transport': '\\jquery/jquery.iframe-transport',
+            'log4javascript': 'log4javascript/log4javascript.js'
+        }
+
+    },
+    // what comes as external dependencies
+    externals: {
+        "jquery": "$",
+        "backbone": "Backbone"
+    },
+    plugins: [
+        // The ProvidePlugin replaces a symbol in another source through the respective import
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+            React: "react"
+        }),
+        // Webpack plugin that will make the process return status code 1 when it finishes with errors in single-run mode.
+        webpackFailPlugin,
+        new ExtractTextPlugin("[name].css")
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.ts(x?)$/,
+                loaders: [
+                    'ts-loader'
+                ]
+            },
+            {
+                test: /\.less$/,
+                loader: ExtractTextPlugin.extract("style-loader", "css-loader!less-loader")
+            }
+        ]
+    }
 };
-
-module.exports = extend(true, config, prodConfig);
